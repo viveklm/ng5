c93b1ff87ce558c76c431af18b85e59d0d573e9f
Vivek Aswal <vivek-aswal@bisk.com>
2018-06-05 13:21:31 -0400

    file upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  fileToUpload: File = null;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
   }
 
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    this.uploadFileToActivity();
+  }
+
+  uploadFileToActivity() {
+    this.postFile(this.fileToUpload).subscribe(data => {
+      // do something, if upload success
+    }, error => {
+      this.handleError(error);
+      console.log(error);
+    });
+  }
+
+  postFile(fileToUpload: File): Observable<any> {
+    let username: string = 'Administrator';
+    let password: string = 'Administrator';
+    const endpoint = 'http://localhost:8080/nuxeo/api/v1/upload';
+    const httpOptions = {
+      headers: {
+        'Authorization': "Basic " + btoa(username + ":" + password),
+        "Content-Type": "application/json",
+        "withCredentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE"
+      }
+    };
+    return this.http
+      .post(endpoint, fileToUpload, httpOptions);
+  }
+
+  private handleError(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
 }
+
